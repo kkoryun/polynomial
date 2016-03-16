52dcd8eb70275402d74ae2612fe38c60eaa1f2be
Koryun <koryun@live.com>
2016-03-16 13:39:19 +0300

    1

---
+++ polynomial/polynomial/cyclelist.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "monom.h"
+class cyclelist : public momon
+{
+public:
+	cyclelist();
+
+
+
+	cyclelist operator+ (const cyclelist & tmp) {
+		cyclelist result(tmp);
+
+		monom *
+
+	}
+
+
+
+	~cyclelist();
+};
+
